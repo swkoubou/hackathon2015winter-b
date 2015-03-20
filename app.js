fb921a30c6013af5d25149d0ca09539dea55a720
sunya <mugen.Xyz@gmail.com>
2015-03-20 17:56:37 +0900

    ゲームタイトルをExpress内でどこでも取得できるようにした。

--- app.js
+++ app.js
@@ -21,6 +21,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.locals = {
+  app: {
+    game_title: "Game"
+  }
+};
+
 app.use('/', routes);
 app.use('/game', routes)
 
