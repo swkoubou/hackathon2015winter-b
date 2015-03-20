7cabb93960e726f440ed0ddee93b7a6e09396402
sunya <mugen.Xyz@gmail.com>
2015-03-20 18:20:31 +0900

    マージされたところ

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var config = require('config/development.json');
+var config = require('../config/default.json');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
