44535cf6a81d347ebb56532627a7b3205a1e87b1
snakazawa <nakazawa.tms0314@gmail.com>
2015-03-20 18:38:50 +0900

    join, leave robyの実装

--- public/javascripts/test/socket.js
+++ public/javascripts/test/socket.js
@@ -1,12 +1,31 @@
 (function () {
+    'use strict';
+
     var socket = io.connect();
 
     socket.on('connect', function () {
         console.log('connect socket.io', arguments);
+        joinRobby();
     });
 
     socket.on('disconnect', function () {
         console.log('disconnect socket.io', arguments);
     });
+
+    socket.on('join-room', function (res) {
+        console.log('join-room', res);
+    });
+
+    socket.on('leave-room', function (res) {
+        console.log('leave-room', res);
+    });
+
+    function joinRobby() {
+        socket.emit('join-robby');
+    }
+
+    function leaveRobby() {
+        socket.emit('leave-robby');
+    }
 
 }());
