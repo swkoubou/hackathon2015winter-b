5f4cd7765fe17d983e4b70b8abf1cca3f4a9a832
sunya <mugen.Xyz@gmail.com>
2015-03-20 17:57:23 +0900

    共有した変数を表示するようにしたり外部ライブラリの取り込み。

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
-var app = require('../app');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 router.get('/game', function(req, res, next){
-  res.render('game', app.GAME_NAME);
+  res.render('game', res.locals);
 });
 
 module.exports = router;
