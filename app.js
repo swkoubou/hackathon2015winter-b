e61cf7518765c962867ed04cb2deaee116046152
sunya <mugen.Xyz@gmail.com>
2015-03-20 14:09:12 +0900

    ゲームページ追加。

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-
+var gamePage = require('./routes/index')
 var app = express();
 
 // view engine setup
@@ -22,6 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+app.use('/game', routes)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
